2507c570a932a93209abdc4a8b9d23e4ad128c42
Sergio Junior <arantes.sergi0h@gmail.com>
2018-12-01 10:50:35 -0200

    Initial commit.

---
+++ app/src/main/java/client/github/ar/com/br/githubclient/models/Owner.java
@@ -0,0 +1,32 @@
+package client.github.ar.com.br.githubclient.models;
+
+public class Owner {
+
+    private Integer id;
+    private String name;
+    private String avatar;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
---
+++ app/src/main/java/client/github/ar/com/br/githubclient/models/Repo.java
@@ -0,0 +1,88 @@
+package client.github.ar.com.br.githubclient.models;
+
+import java.util.Date;
+
+public class Repo {
+
+    private Integer id;
+    private String name;
+    private Date updatedAt;
+    private Integer stargazersCount;
+    private Integer forksCount;
+    private Integer watchersCount;
+    private Integer openIssuesCount;
+    private String licenseName;
+    private Owner owner;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getStargazersCount() {
+        return stargazersCount;
+    }
+
+    public void setStargazersCount(Integer stargazersCount) {
+        this.stargazersCount = stargazersCount;
+    }
+
+    public Integer getForksCount() {
+        return forksCount;
+    }
+
+    public void setForksCount(Integer forksCount) {
+        this.forksCount = forksCount;
+    }
+
+    public Integer getWatchersCount() {
+        return watchersCount;
+    }
+
+    public void setWatchersCount(Integer watchersCount) {
+        this.watchersCount = watchersCount;
+    }
+
+    public Integer getOpenIssuesCount() {
+        return openIssuesCount;
+    }
+
+    public void setOpenIssuesCount(Integer openIssuesCount) {
+        this.openIssuesCount = openIssuesCount;
+    }
+
+    public String getLicenseName() {
+        return licenseName;
+    }
+
+    public void setLicenseName(String licenseName) {
+        this.licenseName = licenseName;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
